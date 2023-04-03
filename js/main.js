81e5170dd4750dfd28e483b5c623ab727dd0d48c
kailahk <kailahkorsh@me.com>
2023-04-03 16:11:37 -0700

    cell objects rendering with all properties

--- js/main.js
+++ js/main.js
@@ -1,5 +1,27 @@
 /*----- constants -----*/
+const cols = [
+    [0, 9, 18, 27, 36, 45, 54, 63, 72],
+    [1, 10, 19, 28, 37, 46, 55, 64, 73],
+    [2, 11, 20, 29, 38, 47, 56, 65, 74],
+    [3, 12, 21, 30, 39, 48, 57, 66, 75],
+    [4, 13, 22, 31, 40, 49, 58, 67, 76],
+    [5, 14, 23, 32, 41, 50, 59, 68, 77],
+    [6, 15, 24, 33, 42, 51, 60, 69, 78],
+    [7, 16, 25, 34, 43, 52, 61, 70, 79],
+    [8, 17, 26, 35, 44, 53, 62, 71, 80]
+]
 
+const boxes = [
+    [0, 1, 2, 9, 10, 11, 18, 19, 20],
+    [3, 4, 5, 12, 13, 14, 21, 22, 23],
+    [6, 7, 8, 15, 16, 17, 24, 25, 26],
+    [27, 28, 29, 36, 37, 38, 45, 46, 47],
+    [30, 31, 32, 39, 40, 41, 48, 49, 50],
+    [33, 34, 35, 42, 43, 44, 51, 52, 53],
+    [54, 55, 56, 63, 64, 65, 72, 73, 74],
+    [57, 58, 59, 66, 67, 68, 75, 76, 77],
+    [60, 61, 62, 69, 70, 71, 78, 79, 80]
+]
 
 /*----- state variables -----*/
 let timer = '00:00'
@@ -7,7 +29,8 @@ let winner = false
 let keyboardType = false // false = normal, true = candidate
 let highlightedCell = [0, 0]
 let answer = [];
-let board = [];
+let numsToShow = [];
+const board = {};
 
 // below code sourced from: https://www.geeksforgeeks.org/program-sudoku-generator/
 class Sudoku {
@@ -150,7 +173,7 @@ class Sudoku {
         for (let i = 0; i < this.N; i++) {
             answer.push([...this.mat[i]])
         }
-        console.log(answer)
+        console.log('answer', answer)
     }
 
     // Remove the K no. of digits to
@@ -167,11 +190,13 @@ class Sudoku {
                 this.mat[i][j] = 0;
             }
         }
-        board = this.mat
-        console.log(board)
+        numsToShow = this.mat
+        console.log('board', numsToShow)
         return;
     }
 }
+
+
 
 
 
@@ -191,6 +216,52 @@ function init() {
         sudoku.fillValues()
         sudoku.generateBoard()
         sudoku.removeKDigits()
+        initCells()
+    }
+
+    function initCells() {
+        let i = 0;
+        answer.forEach(function (row, rowIdx) {
+            row.forEach(function (boardCell, boardCellIdx) {
+                board[`R${rowIdx}C${boardCellIdx}`] = {
+                    'id': i,
+                    'value': boardCell,
+                    'numToShow': numsToShow[rowIdx][boardCellIdx] === 0 ? 0 : boardCell,
+                    'revealed': numsToShow[rowIdx][boardCellIdx] ? true : false,
+                    'candidates': {
+                        1: false,
+                        2: false,
+                        3: false,
+                        4: false,
+                        5: false,
+                        6: false,
+                        7: false,
+                        8: false,
+                        9: false
+                    },
+                    'row': rowIdx,
+                }
+                checkColandBox(rowIdx, boardCellIdx, i)
+                i++
+            })
+        })
+        console.log(board)
+    }
+
+    function checkColandBox(rowNum, cellNum, id) {
+        let currCell = board[`R${rowNum}C${cellNum}`]
+
+        cols.forEach(function (column, colIdx) {
+            if (column.includes(id)) {
+                currCell['col'] = colIdx;
+            } 
+        });
+        boxes.forEach(function (box, boxIdx) {
+            if (box.includes(id)) {
+                console.log(box, boxIdx, id)
+                currCell['box'] = boxIdx
+            }
+        });
     }
 
     initBoard();
