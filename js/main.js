b1b6ad53c81cdfcbe25bf0b6200fc7343cc3baad
kailahk <kailahkorsh@me.com>
2023-04-18 14:11:56 -0700

    cleans up comments in Sudoku class creation

--- js/main.js
+++ js/main.js
@@ -37,17 +37,13 @@ const board = {};
 // code for Sudoku class sourced from: https://www.geeksforgeeks.org/program-sudoku-generator/
 class Sudoku {
 
-    // Constructor
     constructor(N, K) {
         this.N = N;
         this.K = K;
 
-        // Compute square root of N
         const SRNd = Math.sqrt(N);
         this.SRN = Math.floor(SRNd);
 
-        // Initialize all entries as false to indicate
-        // that there are no edges initially
         this.mat = Array.from({
             length: N
         }, () => Array.from({
@@ -55,17 +51,13 @@ class Sudoku {
         }, () => 0));
     }
 
-    // Sudoku Generator
     fillValues() {
-        // Fill the diagonal of SRN x SRN matrices
         this.fillDiagonal();
 
-        // Fill remaining blocks
         this.fillRemaining(0, this.SRN);
 
     }
 
-    // Fill the diagonal SRN number of SRN x SRN matrices
     fillDiagonal() {
         for (let i = 0; i < this.N; i += this.SRN) {
             // for diagonal box, start coordinates->i==j
@@ -73,7 +65,6 @@ class Sudoku {
         }
     }
 
-    // Returns false if given 3 x 3 block contains num.
     unUsedInBox(rowStart, colStart, num) {
         for (let i = 0; i < this.SRN; i++) {
             for (let j = 0; j < this.SRN; j++) {
@@ -85,7 +76,6 @@ class Sudoku {
         return true;
     }
 
-    // Fill a 3 x 3 matrix.
     fillBox(row, col) {
         let num = 0;
         for (let i = 0; i < this.SRN; i++) {
@@ -101,12 +91,10 @@ class Sudoku {
         }
     }
 
-    // Random generator
     randomGenerator(num) {
         return Math.floor(Math.random() * num + 1);
     }
 
-    // Check if safe to put in cell
     checkIfSafe(i, j, num) {
         return (
             this.unUsedInRow(i, num) &&
@@ -115,7 +103,6 @@ class Sudoku {
         );
     }
 
-    // check in the row for existence
     unUsedInRow(i, num) {
         for (let j = 0; j < this.N; j++) {
             if (this.mat[i][j] === num) {
@@ -125,7 +112,6 @@ class Sudoku {
         return true;
     }
 
-    // check in the row for existence
     unUsedInCol(j, num) {
         for (let i = 0; i < this.N; i++) {
             if (this.mat[i][j] === num) {
@@ -135,27 +121,20 @@ class Sudoku {
         return true;
     }
 
-    // A recursive function to fill remaining
-    // matrix
     fillRemaining(i, j) {
-        // Check if we have reached the end of the matrix
         if (i === this.N - 1 && j === this.N) {
             return true;
         }
 
-        // Move to the next row if we have reached the end of the current row
         if (j === this.N) {
             i += 1;
             j = 0;
         }
 
-
-        // Skip cells that are already filled
         if (this.mat[i][j] !== 0) {
             return this.fillRemaining(i, j + 1);
         }
 
-        // Try filling the current cell with a valid value
         for (let num = 1; num <= this.N; num++) {
             if (this.checkIfSafe(i, j, num)) {
                 this.mat[i][j] = num;
@@ -166,24 +145,19 @@ class Sudoku {
             }
         }
 
-        // No valid value was found, so backtrack
         return false;
     }
 
-    // Print sudoku
     generateBoard() {
         for (let i = 0; i < this.N; i++) {
             answer.push([...this.mat[i]])
         }
     }
 
-    // Remove the K no. of digits to
-    // complete game
     removeKDigits() {
         let count = this.K;
 
         while (count !== 0) {
-            // extract coordinates i and j
             let i = Math.floor(Math.random() * this.N);
             let j = Math.floor(Math.random() * this.N);
             if (this.mat[i][j] !== 0) {
@@ -195,7 +169,6 @@ class Sudoku {
         return;
     }
 }
-
 
 
 /*----- cached elements  -----*/
